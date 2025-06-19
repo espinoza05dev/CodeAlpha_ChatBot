52d071379c7e7365219d1524d57854f81451ef4b
Sbrentor <githubpersonal914@gmail.com>
2025-06-18 23:01:09 -0600

    .

--- Components/inputs_process.py
+++ Components/inputs_process.py
@@ -1,6 +1,6 @@
 class In_OutPut:
     def CleanInput(self,text):
-        return text.lower().strip().replace(' ','_').replace('!','').replace('?','')
+        return "".join(text.lower().strip().replace(' ','_').replace('!','').replace('?',''))
 
     def tokenize(self,text):
-        return text.split()
+        return "".join(text.split())
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from Components.inputs_process import In_OutPut
+from Components.inputs_process import In_OutPut as IO
+
+def chatbot():
+    chat = True
+    clean_In = IO()
+    token = IO()
+    print("WELLCOME I AM YOUR CHAT BOT ")
+    print("WRITE ANYTHING I GOING TO TRY TO HELP YOU")
+    while chat:
+        msg = token.tokenize(clean_In.CleanInput(input("Write something: ")))
+
+        if msg == 'exit': chat = False
+
 if __name__ == "__main__":
-
-
-    print("hola")
+    chatbot()
