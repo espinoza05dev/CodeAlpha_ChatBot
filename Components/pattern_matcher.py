3b8d773936f78728f61a030d5fd1d091ac99b09d
Sbrentor <githubpersonal914@gmail.com>
2025-06-20 16:55:00 -0600

    .

--- Components/pattern_matcher.py
+++ Components/pattern_matcher.py
@@ -1,7 +1,22 @@
-from json_data import JsonData as JD
-class PatternMatcher:
-    def __init__(self, cmd):
-        self.patterns = JD.Patterns(cmd)
+import json
+from random import choice
+COMMANDS = {
+    1: "greeting", 2: "goodbye", 3: "help", 4: "thanks", 5: "unknown", 6: "yes", 7: "no", 8: "apologize", 9: "compliment", 10: "complaint",
+    11: "small_talk", 12: "contact", 13: "hours", 15: "weather", 16: "name", 17: "capability", 18: "joke", 19: "repeat", 20: "unclear",
+    21:"fallback_responses",22:"conversation_starters"
+}
 
+path_pattern = r"../data_json/patterns.json"
+
+class PatternMatcher:
     def match(self, cmd):
-        return self.patterns.get(cmd)
+        with open(path_pattern, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            if cmd in COMMANDS:
+                return choice(data["patterns"][COMMANDS[cmd]]["keywords"])
+            else:
+                return ""
+
+if __name__ == '__main__':
+    res = PatternMatcher()
+    print(res.match(1))
--- Components/rule_engine.py
+++ Components/rule_engine.py
@@ -1,10 +1,19 @@
-from json_data import JsonData as JD
-class RuleEngine:
-    def __init__(self):
-        self.pattern = JD.Responses_Template()
+import json
+from random import choice
+COMMANDS = {
+    1: "greeting", 2: "goodbye", 3: "help", 4: "thanks", 5: "unknown", 6: "yes", 7: "no", 8: "apologize", 9: "compliment", 10: "complaint",
+    11: "small_talk", 12: "contact", 13: "hours", 15: "weather", 16: "name", 17: "capability", 18: "joke", 19: "repeat", 20: "unclear",
+    21:"fallback_responses",22:"conversation_starters"
+}
 
-    def apply_rules(self, cmd):
-        return  self.pattern.get(cmd)
+path_template = r"../data_json/chatbot_responses_template.json"
+
+class RuleEngine:
+    def apply_rules(self,cmd):
+        with open(path_template,"r",encoding="utf-8") as f:
+            data = json.load(f)
+            if cmd in COMMANDS:return choice(data["response_templates"][COMMANDS[cmd]]["responses"])
+            else: return ""
 
 if __name__ == '__main__':
     res = RuleEngine()
